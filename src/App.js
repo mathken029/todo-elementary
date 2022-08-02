37b3d1be506ff3285ac6b2ce74b9fddf57a7f07e
@_mathken <dummy@example.com>
2022-08-02 10:08:06 +0900

    編集機能を追加しました

--- src/App.js
+++ src/App.js
@@ -3,38 +3,33 @@ import { useState } from "react";
 function App() {
   // # 上から順番に機能追加していきます
 
-  // ## TODO項目への詳細の追加
-  // * TODOの作成の際に、入力が任意の詳細を入力するためのテキストボックスを追加します
-  // * TODO項目のタイトルと同様にラベルで表示します
-
-  // ## TODOの編集機能の追加
+  // ## TODOの削除機能の追加
   // * 過去のソースコードを参考に追加します
-  // * ボタン「編集」を入力した際に詳細も編集できるようにします
 
   // # 以下は、余裕があれば実施する項目です
+  // ## タイトルのみ必須で詳細は任意とする
+  // * 現状、タイトル及び詳細の両方が入力されていないとtrim()でエラーになりますが、タイトルのみを入力必須にしたいです
+
   // ## ソート(ID、期限、ステータスで並べ替え)の追加
   // ## ステータス変更でスタイル変更（どういう動作かを確認する）
   // ## どれかのパーツのコンポーネント化
 
   const [todos, setTodos] = useState([]);
-  const [todoTitle, setTodoTitle] = useState("");
-  const [todoDetail, setTodoDetail] = useState("");
+  const [todoTitle, setTodoTitle] = useState({});
   const [isEditing, setIsEditing] = useState(false);
-  const [currentTodo, setCurrentTodo] = useState({});
+  const [currentTodoTitle, setCurrentTodoTitle] = useState({});
   const [idCount, setIdCount] = useState(1);
 
   function handleAddTodoTitleInputChange(e) {
-    setTodoTitle(e.target.value);
+    setTodoTitle({ ...todoTitle, title: e.target.value });
   }
 
   function handleAddTodoDetailInputChange(e) {
-    setTodoDetail(e.target.value);
+    setTodoTitle({ ...todoTitle, detail: e.target.value });
   }
 
   function handleAddFormSubmit(e) {
     e.preventDefault();
-    console.log("title:" + todoTitle);
-    console.log("detail:" + todoDetail);
 
     if (todoTitle !== "") {
       setIdCount(idCount + 1);
@@ -43,14 +38,45 @@ function App() {
         {
           id: idCount,
           timestamp: new Date(),
-          title: todoTitle.trim(),
-          detail: todoDetail.trim(),
+          title: todoTitle.title.trim(),
+          detail: todoTitle.detail.trim(),
         },
       ]);
     }
 
-    setTodoTitle("");
-    setTodoDetail("");
+    setTodoTitle({ ...todoTitle, title: "", detail: "" });
+  }
+
+  function handleEditClick(todo) {
+    setIsEditing(true);
+    setCurrentTodoTitle({ ...todo });
+    console.log(todo.id);
+    console.log(currentTodoTitle.id);
+  }
+
+  function handleEditTodoTitleInputChange(e) {
+    setCurrentTodoTitle({ ...currentTodoTitle, title: e.target.value });
+  }
+
+  function handleEditTodoDetailInputChange(e) {
+    setCurrentTodoTitle({ ...currentTodoTitle, detail: e.target.value });
+  }
+
+  function handleEditFormSubmit(e) {
+    e.preventDefault();
+
+    const updatedItem = todos.map((todo) => {
+      console.log(todo.id);
+      console.log(currentTodoTitle.id);
+
+      if (todo.id === currentTodoTitle.id) {
+        return currentTodoTitle;
+      } else {
+        return todo;
+      }
+    });
+    setIsEditing(false);
+    setTodos(updatedItem);
   }
 
   return (
@@ -58,27 +84,41 @@ function App() {
       {isEditing ? (
         <form>
           <h2>TODOの編集</h2>
-          <label>TODOの更新: </label>
-          <input />
-          <button>更新</button>
-          <button>キャンセル</button>
+          <label htmlFor="editTodoTitle">TODOのタイトル: </label>
+          <input
+            name="editTodoTitle"
+            type="text"
+            value={currentTodoTitle.title}
+            onChange={handleEditTodoTitleInputChange}
+          />
+
+          <label htmlFor="editTodoTitle">詳細(任意): </label>
+          <input
+            name="editTodoTitle"
+            type="text"
+            value={currentTodoTitle.detail}
+            onChange={handleEditTodoDetailInputChange}
+          />
+
+          <button onClick={handleEditFormSubmit}>更新</button>
+          <button onClick={() => setIsEditing(false)}>キャンセル</button>
         </form>
       ) : (
         <form onSubmit={handleAddFormSubmit}>
           <h2>TODOの追加</h2>
-          <label htmlFor="todoTitle">TODOのタイトル: </label>
+          <label htmlFor="AddTodoTitle">TODOのタイトル: </label>
           <input
-            name="todoTitle"
+            name="AddTodoTitle"
             type="text"
-            value={todoTitle}
+            value={todoTitle.title}
             onChange={handleAddTodoTitleInputChange}
           />
 
-          <label htmlFor="todoDetail">詳細(任意): </label>
+          <label htmlFor="AddTodoDetail">詳細(任意): </label>
           <input
-            name="todoDetail"
+            name="AddTodoDetail"
             type="text"
-            value={todoDetail}
+            value={todoTitle.detail}
             onChange={handleAddTodoDetailInputChange}
           />
 
@@ -95,8 +135,10 @@ function App() {
               <option>進行中</option>
               <option>完了</option>
             </select>
-            <button>編集</button>
-            <button>削除</button>
+            <button onClick={() => handleEditClick(todo)}>編集</button>
+            {/* <button onClick={() => handleDeleteClick(todo.timestamp)}>
+              削除
+            </button> */}
           </li>
         ))}
       </ul>
